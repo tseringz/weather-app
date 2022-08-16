9fea3f14bb5fbe53310cd0de0c724dd9b17bb3c8
Tsering Dhondup <tserockon77@hotmail.com>
2022-08-16 23:10:16 +0530

    Add color palette and setup layout

--- src/weather.js
+++ src/weather.js
@@ -2,7 +2,6 @@
 async function getWeather() {
     let currentWeather  = await fetch('http://api.openweathermap.org/data/2.5/weather?q=London&APPID=38a9b80676d2854b1f7e81bf63d6e1f6', {mode: 'cors'});
     let result = await currentWeather.json();
-
     console.log(result);
 }
 
